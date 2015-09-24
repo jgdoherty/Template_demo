a638f5ddbb11fe25dffa512a0c22d0206de85cf7
jgdoherty <jmdoherty@eircom.net>
2015-09-24 15:41:35 +0100

    Create myModule.js

---
+++ modules/myModule.js
@@ -0,0 +1,27 @@
+
+
+ angular.module('myApp',[
+'ngRoute']).
+config(['$routeProvider',
+function($routeProvider){
+
+	$routeProvider.
+		when('/home',{
+			templateUrl:'http://localhost:8080/Lab/partials/basic-template.html',
+			controller:'Basicontroller'
+
+		}).
+		otherwise({
+                    redirectTo: '/home'
+                });
+
+}]);
+
+
+
+function Basicontroller($scope){
+	
+$scope.title = 'this is the title';
+$scope.body = 'Welcome...';
+
+}
